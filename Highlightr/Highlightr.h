0a63fc84e08ee0911c5fefb210c6e0328b1e9c53
Rajdeep Kwatra <rajdeep.kwatra@yahoo.com>
2017-01-16 11:03:57 +1100

    Added Shared scheme to support build via Carthage

---
+++ Highlightr/Highlightr.h
@@ -0,0 +1,19 @@
+//
+//  Highlightr.h
+//  Highlightr
+//
+//  Created by Rajdeep Kwatra on 1/16/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Highlightr.
+FOUNDATION_EXPORT double HighlightrVersionNumber;
+
+//! Project version string for Highlightr.
+FOUNDATION_EXPORT const unsigned char HighlightrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Highlightr/PublicHeader.h>
+
+
